f8299e03bee1964f69c2ad6662f961ecc6c24d0a
wil0420 <wil.martinez0420@gmail.com>
2020-04-11 20:08:51 -0500

    Procedural

---
+++ Assets/scripts/Bg_scroll.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bg_scroll : MonoBehaviour
+{
+
+    public float Scroll_Speed = 0.1f;
+    private MeshRenderer mesh_Renderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mesh_Renderer = GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Time.time * Scroll_Speed;
+
+        Vector2 offset = new Vector2(x, 0);
+
+        mesh_Renderer.sharedMaterial.SetTextureOffset("_MainTex", offset);
+    }
+}
